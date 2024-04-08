76a40013dffb67348cffd7732a70b00ea085250b
Morris Ombiro <morriskalb@gmail.com>
2024-04-08 13:19:31 -0500

    UI

--- src/components/current_value_context.tsx
+++
@@ -1,5 +0,0 @@
-import {createContext} from 'react'
-
-const CurrentValue = createContext(0);
-
-export default CurrentValue; 
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,21 +3,32 @@ import styled from "styled-components";
 import { useCoinbaseResp } from "./CoinbaseProvider/hooks";
 import {
   Box,
+  Button,
   createTheme,
+  FormControlLabel,
   Grid,
   IconButton,
   MuiThemeProvider,
   Paper,
+  Switch,
   TextField,
   Tooltip,
   Typography,
 } from "@material-ui/core";
-import { Add, AddAlert, CancelSharp } from "@mui/icons-material";
+import {
+  Add,
+  AddAlert,
+  Alarm,
+  AlarmOff,
+  AlarmOn,
+  CancelSharp,
+} from "@mui/icons-material";
 import { Close } from "@material-ui/icons";
 import { Divider } from "semantic-ui-react";
 import success from "./notifications/success.mp3";
 import fail from "./notifications/fail.mp3";
 import useSound from "use-sound";
+import { BitcoinUSD } from "./CoinbaseProvider/context";
 
 const Logo = styled.h1`
   font-size: 1.8em;
@@ -41,25 +52,37 @@ const LogoTiny = styled.h1`
   }
 `;
 
+const vibratingAnimation = {
+  "0%": { transform: "translateX(0)" },
+  "20%": { transform: "translateX(-5px)" },
+  "40%": { transform: "translateX(5px)" },
+  "60%": { transform: "translateX(-5px)" },
+  "80%": { transform: "translateX(5px)" },
+  "100%": { transform: "translateX(0)" },
+};
+
 const Main: React.FC = () => {
   const { bitcoinPrice, userPrice, setUserPrice } = useCoinbaseResp();
-  const [dialogOpen, setDialogOpen] = React.useState(false);
-  const [play] = useSound(success);
-  const [playFail] = useSound(fail);
+  const [dialogOpen, setDialogOpen] = React.useState(true);
+  const successAudio = new Audio(success);
+  const failAudio = new Audio(fail);
 
   React.useEffect(() => {
     setUserPrice(localStorage.getItem("currentUserPrice") as unknown as number);
   }, [userPrice]);
+  const [playSound, setPlaySound] = React.useState(false);
 
   React.useEffect(() => {
-    if (userPrice && bitcoinPrice) {
-      console.log("ay");
-      if (userPrice > bitcoinPrice.rate_float) {
-        play();
+    if (playSound) {
+      if (userPrice && bitcoinPrice) {
+        if (userPrice < bitcoinPrice?.rate_float) {
+          successAudio.play();
+          return;
+        }
+        failAudio.play();
       }
-      playFail();
     }
-  }, [userPrice, bitcoinPrice]);
+  }, [userPrice, bitcoinPrice?.rate_float, playSound]);
 
   const theme = createTheme({
     overrides: {
@@ -73,182 +96,192 @@ const Main: React.FC = () => {
     },
   });
 
+  const difference =
+    bitcoinPrice && userPrice ? bitcoinPrice.rate_float - userPrice : 0;
+
   return (
-    <>
+    <div
+      style={{
+        height: "100%",
+        position: "relative",
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+        padding: "200px",
+      }}
+    >
       <Grid
         container
-        direction="column"
         alignItems="center"
         justifyContent="center"
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: " 20%",
-          marginTop: "-50px",
-          marginLeft: "-50px",
-          width: "700px",
-          height: "100px",
-        }}
+        style={{ width: "1000px" }}
       >
-        {dialogOpen ? (
-          <Grid container spacing={1}>
-            <Grid
-              item
-              container
-              justifyContent="space-between"
-              alignItems="center"
+        <Grid item container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Logo>
+              Casual<span>Crypto</span>
+            </Logo>
+            <Typography variant="h1" color="textSecondary">
+              ${bitcoinPrice?.rate}
+            </Typography>
+            <Box
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
             >
-              <Grid item>
-                <Logo>
-                  Casual<span>Crypto</span>
-                </Logo>
-              </Grid>
-              <Grid item>
-                <Typography variant="body2">${bitcoinPrice?.rate}</Typography>
-                <Typography variant="body2" color="textSecondary">
-                  BTC
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Grid container spacing={2}>
-                <Grid container item justifyContent="space-between">
-                  <TextField
-                    id="standard-helperText"
-                    label="Avg. BTC buy-in"
-                    helperText="Enter as number; e.g: 69420"
-                    variant="standard"
-                    type="number"
-                    value={
-                      userPrice ??
-                      (localStorage.getItem(
-                        "currentUserPrice"
-                      ) as unknown as number)
-                    }
-                    onChange={(e) => {
-                      setUserPrice(e.target.value as unknown as number);
-                      localStorage.setItem("currentUserPrice", e.target.value);
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                style={{ marginRight: "24px" }}
+              >
+                current BITCOIN price
+              </Typography>
+              <FormControlLabel
+                control={
+                  <Switch
+                    aria-label="sound-check"
+                    icon={<AlarmOff />}
+                    color="primary"
+                    checkedIcon={<AlarmOn />}
+                    size="medium"
+                    onClick={() => {
+                      setPlaySound((prev) => !prev);
                     }}
                   />
-                  {bitcoinPrice && userPrice && (
-                    <Typography
-                      variant="subtitle1"
-                      color={
-                        bitcoinPrice.rate_float - userPrice > 0
-                          ? "primary"
-                          : "error"
-                      }
-                    >
-                      <strong>
-                        {`$${(
-                          bitcoinPrice.rate_float - userPrice
-                        ).toLocaleString(undefined, {
-                          minimumFractionDigits: 2,
-                          maximumFractionDigits: 2,
-                        })}`}
-                      </strong>
-                    </Typography>
-                  )}
-                </Grid>
-                <Grid item>
-                  <Typography variant="body2" color="textSecondary">
-                    Track the value of your Bitcoin investments effortlessly
-                    with our BTC stock value tracker. Input your average buy-in
-                    price and stay updated on the real-time market value of your
-                    holdings. Whether you're a seasoned investor or just
-                    starting out, our user-friendly interface makes it easy to
-                    monitor your BTC portfolio's performance and make informed
-                    decisions. Keep tabs on fluctuations in the cryptocurrency
-                    market and seize opportunities to optimize your investment
-                    strategy. Start tracking your Bitcoin journey today and take
-                    control of your financial future.
+                }
+                label={
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    style={{ fontSize: "12px" }}
+                  >
+                    Sound notification
                   </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
+                }
+              />
+            </Box>
           </Grid>
-        ) : (
+          <Grid item>
+            <MuiThemeProvider theme={theme}>
+              <Tooltip
+                title={
+                  <Box
+                    style={{
+                      margin: "8px",
+                    }}
+                  >
+                    <Box
+                      style={{
+                        display: "flex",
+                      }}
+                    >
+                      <AddAlert color="info" style={{ marginRight: "8px" }} />
+                      <Typography variant="body2">
+                        Modify your average buy-in price to start getting
+                        notifications
+                      </Typography>
+                    </Box>
+                    <Divider style={{ marginBottom: "8px" }} />
+                    <LogoTiny>
+                      Casual<span>Crypto</span>
+                    </LogoTiny>
+                  </Box>
+                }
+                placement="top-end"
+                enterDelay={1}
+                leaveDelay={1}
+              >
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    setDialogOpen((prev) => !prev);
+                  }}
+                  style={{
+                    outline: "none",
+                  }}
+                  startIcon={
+                    <Add
+                      fontSize="large"
+                      color={difference > 0 ? "success" : "error"}
+                    />
+                  }
+                >
+                  {!dialogOpen
+                    ? "Update average buy-in"
+                    : "Hide average buy-in"}
+                </Button>
+              </Tooltip>
+            </MuiThemeProvider>
+          </Grid>
+        </Grid>
+        {dialogOpen && (
           <Grid
             item
             container
             justifyContent="space-between"
             alignItems="center"
+            style={{ marginTop: "24px" }}
           >
-            <Grid item>
-              <Logo>
-                Casual<span>Crypto</span>
-              </Logo>
-              <Typography variant="h1" color="textSecondary">
-                ${bitcoinPrice?.rate}
-              </Typography>
-              <Typography variant="body2" color="textSecondary">
-                current BITCOIN price
-              </Typography>
+            <Grid container item justifyContent="space-between">
+              <Grid
+                container
+                item
+                justifyContent="space-between"
+                style={{ marginBottom: "12px" }}
+              >
+                <TextField
+                  id="standard-helperText"
+                  label="Avg. BTC buy-in"
+                  helperText="Enter as number; e.g: 69420"
+                  variant="standard"
+                  type="number"
+                  value={
+                    userPrice ??
+                    (localStorage.getItem(
+                      "currentUserPrice"
+                    ) as unknown as number)
+                  }
+                  onChange={(e) => {
+                    setUserPrice(e.target.value as unknown as number);
+                    localStorage.setItem("currentUserPrice", e.target.value);
+                  }}
+                />
+                {bitcoinPrice && userPrice && (
+                  <Typography
+                    variant="subtitle1"
+                    color={difference > 0 ? "primary" : "error"}
+                  >
+                    <strong>
+                      {`Net return: $${(
+                        bitcoinPrice.rate_float - userPrice
+                      ).toLocaleString(undefined, {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                      })}`}
+                    </strong>
+                  </Typography>
+                )}
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" color="textSecondary">
+                  Track the value of your Bitcoin investments effortlessly with
+                  our BTC stock value tracker. Input your average buy-in price
+                  and stay updated on the real-time market value of your
+                  holdings. Whether you're a seasoned investor or just starting
+                  out, our user-friendly interface makes it easy to monitor your
+                  BTC portfolio's performance and make informed decisions. Keep
+                  tabs on fluctuations in the cryptocurrency market and seize
+                  opportunities to optimize your investment strategy. Start
+                  tracking your Bitcoin journey today and take control of your
+                  financial future.
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
         )}
-        <div style={{ marginLeft: "16px" }}>
-          {dialogOpen ? (
-            <Grid item>
-              <IconButton
-                onClick={() => {
-                  setDialogOpen(false);
-                }}
-                style={{ outline: "none" }}
-              >
-                <CancelSharp fontSize="large" color="primary" />
-              </IconButton>
-            </Grid>
-          ) : (
-            <Grid item>
-              <IconButton
-                onClick={() => {
-                  setDialogOpen(true);
-                }}
-                style={{
-                  outline: "none",
-                }}
-              >
-                <MuiThemeProvider theme={theme}>
-                  <Tooltip
-                    title={
-                      <Box
-                        style={{
-                          margin: "8px",
-                        }}
-                      >
-                        <Box
-                          style={{
-                            display: "flex",
-                          }}
-                        >
-                          <AddAlert
-                            color="info"
-                            style={{ marginRight: "8px" }}
-                          />
-                          <Typography variant="body2">
-                            Modify your average buy-in price to start getting
-                            notifications
-                          </Typography>
-                        </Box>
-                        <Divider style={{ marginBottom: "8px" }} />
-                        <LogoTiny>
-                          Casual<span>Crypto</span>
-                        </LogoTiny>
-                      </Box>
-                    }
-                    placement="top-start"
-                    open
-                  >
-                    <Add fontSize="large" color="primary" />
-                  </Tooltip>
-                </MuiThemeProvider>
-              </IconButton>
-            </Grid>
-          )}
-        </div>
       </Grid>
-    </>
+    </div>
   );
 };
 
